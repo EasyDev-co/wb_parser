7e323548ad5611c3e4251f799c5e34c3aa5bccdb
Sherif Ragimov <ragimov700@yandex.ru>
2024-04-05 18:30:25 +0400

    resolving conflicts

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+import sentry_sdk
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -148,3 +149,9 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 
 TELEGRAM_API_URL = 'https://api.telegram.org/bot{}/sendMessage'
+
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_DSN'),
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
