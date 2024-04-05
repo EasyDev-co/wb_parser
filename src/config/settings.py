bd162cd3c3fdc52f7e8840641e21b5fdce104502
Sherif Ragimov <ragimov700@yandex.ru>
2024-04-05 17:39:21 +0400

    WP-4 parser for the wildberries

--- src/config/settings.py
+++ src/config/settings.py
@@ -120,3 +120,16 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+BASE_URL = 'https://search.wb.ru/exactmatch/ru/common/v5/search?'
+HEADERS = {
+    'Accept': "*/*",
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+}
+ITEMS_PER_PAGE = 30
+MAX_PAGES = 20
+APP_TYPE = 128
+CURRENCY = 'rub'
+LANGUAGE = 'ru'
+DESTINATION = -1257786
+SPP = 30
