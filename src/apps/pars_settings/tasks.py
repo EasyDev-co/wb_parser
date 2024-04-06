9bc98da7f5aa30d1c31bf2a9945d8dd6bf42b0c1
Kamran <kamrankek48@gmail.com>
2024-04-06 19:08:39 +0400

    WP-7: StartParseSendMessageTask refactor and bug fixes

--- src/apps/pars_settings/tasks.py
+++ src/apps/pars_settings/tasks.py
@@ -1,7 +1,27 @@
+from typing import List
+
 from apps.pars_settings.models import Query
 from config.celery import BaseTask, app
+from django.db import transaction
 from service.notify_service import NotifyService
 from service.parsers import ProductPositionParser
+
+from apps.pars_settings.models import Position
+
+
+class QueryUpdater:
+    @staticmethod
+    def update_position(query: Query, updated_position: int) -> Position | None:
+        if updated_position and updated_position != query.target_position:
+            query.target_position = updated_position
+
+            position = Position(
+                query=query,
+                current_position=updated_position,
+                target_position=query.target_position,
+            )
+
+            return position
 
 
 class StartParseSendMessageTask(BaseTask):
@@ -12,27 +32,37 @@ class StartParseSendMessageTask(BaseTask):
         self.notify_service = NotifyService()
         self.product_parser = ProductPositionParser()
 
-    def procces(self):
+    @staticmethod
+    def _bulk_create_positions(positions: List[Position]):
+        with transaction.atomic():
+            Position.objects.bulk_create(positions)
+
+    def process(self):
         queries = Query.objects.select_related('article')
+        new_positions = []
         for query in queries:
             updated_position = self.product_parser.parse_position(
                 query=query.query,
                 article=int(query.article.code)
             )
-            if updated_position and updated_position != query.target_position:
-                query.target_position = updated_position
-                query.positions.update(current_position=updated_position)
-                query.save()
+            position = QueryUpdater.update_position(query, updated_position)
+            if position:
+                new_positions.append(position)
 
-                self.notify_service.send_message(
-                    message=(
-                        f'Товар с артиклом <b>{query.article.code}</b> упал с '
-                        f'<b>{query.target_position}</b> позиции на <b>{updated_position}(ю)</b>'
-                    )
+            self.notify_service.send_message(
+                message=(
+                    f'Товар с артиклом <b>{query.article.code}</b> упал с '
+                    f'<b>{position.target_position}</b> позиции на <b>{updated_position}(ю)</b>'
                 )
+            )
+
+            if len(new_positions) > 2500:
+                with transaction.atomic():
+                    self._bulk_create_positions(new_positions)
+                    new_positions = []
+
+        if new_positions:
+            self._bulk_create_positions(new_positions)
 
 
 start_parse_send_message_task = app.register_task(StartParseSendMessageTask())
-
-
-
