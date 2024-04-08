f00e9a63ef290662123fbf1789ca5b7112ec70fd
KamranYsupov <158745725+KamranYsupov@users.noreply.github.com>
2024-04-08 18:44:37 +0400

    Revert "Feature/wp 7"

--- src/apps/pars_settings/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ParsSettingsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.pars_settings'
-    verbose_name = 'Настройки парсера'
