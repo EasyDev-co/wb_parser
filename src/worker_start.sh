645327eeecfaf78c9ac23b69348ab47123c977dc
Kamran <kamrankek48@gmail.com>
2024-04-15 21:44:02 +0400

    WP-8: deploy refactor

--- src/worker_start.sh
+++ src/worker_start.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 set -e
 
-celery -A config.celery worker --loglevel=info
+celery -A config.celery worker --loglevel=info -P gevent
 
-celery -A config.celery beat --loglevel=info
